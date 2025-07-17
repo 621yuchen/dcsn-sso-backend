f2108a67839da3e27022267c2df65dc15a048077
zhixb <zhixb@neusoft.com>
2025-07-17 15:34:16 +0800

    dev

---
+++ src/main/java/dcsn/sso/config/GlobalCorsConfig.java
@@ -0,0 +1,67 @@
+package dcsn.sso.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author
+ * 全局跨域统一配置
+ */
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOriginPatterns("https://dscn-snow-uat-web.chinacloudsites.cn","https://dscn-snow-web.chinacloudsites.cn") // 允许所有域
+                        .allowedMethods("*") // 允许任何方法（post、get等）
+                        .allowedHeaders("*") // 允许任何请求头
+                        .allowCredentials(true) // 允许证书、cookie
+                        .exposedHeaders(HttpHeaders.SET_COOKIE)
+                        .maxAge(3600L); // maxAge(3600)表明在3600秒内，不需要再发送预检验请求，可以缓存该结果
+            }
+        };
+    }
+
+    /**
+     * 跨域配置
+     */
+    @Bean
+    public CorsFilter corsFilter()
+    {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // 是否允许请求带有验证信息
+        config.setAllowCredentials(true);
+
+        // 允许访问的客户端域名
+        List<String> allowedOriginPatterns = new ArrayList<>();
+        allowedOriginPatterns.add("*");
+        config.setAllowedOriginPatterns(allowedOriginPatterns);
+
+        // 设置访问源地址
+        // config.addAllowedOrigin("*");
+        // 设置访问源请求头
+        config.addAllowedHeader("*");
+        // 设置访问源请求方法
+        config.addAllowedMethod("*");
+        // 对接口配置跨域设置
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
